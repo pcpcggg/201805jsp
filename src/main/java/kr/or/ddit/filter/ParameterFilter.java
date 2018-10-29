d5888dede869836173296411c0dec5d05e90426f
pcpcggg <pcpcggg@gmail.com>
2018-10-29 15:18:17 +0900

    user prod 경료 변경

--- src/main/java/kr/or/ddit/filter/ParameterFilter.java
+++ src/main/java/kr/or/ddit/filter/ParameterFilter.java
@@ -1,41 +1,41 @@
-package kr.or.ddit.filter;
-
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-
-import com.sun.org.apache.xpath.internal.operations.String;
-
-public class ParameterFilter implements Filter {
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		//새로운 파라미터 추가
-		PrameterWrapper pw = new PrameterWrapper((HttpServletRequest)request);
-		
-		pw.setParameter("newParameter", new String[]{"brown","sally","cony"});
-		
-		chain.doFilter(pw, response);
-		
-	}
-
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
+//package kr.or.ddit.filter;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//
+//import com.sun.org.apache.xpath.internal.operations.String;
+//
+//public class ParameterFilter implements Filter {
+//
+//	@Override
+//	public void init(FilterConfig filterConfig) throws ServletException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void doFilter(ServletRequest request, ServletResponse response,
+//			FilterChain chain) throws IOException, ServletException {
+//		//새로운 파라미터 추가
+//		PrameterWrapper pw = new PrameterWrapper((HttpServletRequest)request);
+//		
+//		pw.setParameter("newParameter", new String[]{"brown","sally","cony"});
+//		
+//		chain.doFilter(pw, response);
+//		
+//	}
+//
+//	@Override
+//	public void destroy() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//}
--- src/main/java/kr/or/ddit/filter/PrameterWrapper.java
+++ src/main/java/kr/or/ddit/filter/PrameterWrapper.java
@@ -1,58 +1,58 @@
-package kr.or.ddit.filter;
-
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
-
-public class PrameterWrapper extends HttpServletRequestWrapper {
-	
-	private Map<String, String[]> newMap;
-	
-	public PrameterWrapper(HttpServletRequest request) {
-		super(request);
-		
-		//reuqest 객체의 parmeterMap객체를 복사하여 새로운 map객체를 생성
-		newMap = new HashMap<String, String[]>(request.getParameterMap());
-	}
-	
-	//새로운 파라미터를 저장한다.
-	public void setParameter(String key, String[] values){
-		newMap.put(key, values);
-	}
-	
-	//parameter 관련 메소드 재정의
-	//getParameter(String name);
-	//getParameterValeus(String name);
-	//getParameterMap();
-	//getParameterNames();
-	
-	@Override
-	public Map<String, String[]> getParameterMap() {
-		return newMap;
-	}
-	
-	public String[] getParameterValeus(String name) {
-		return newMap.get(name);
-	}
-	
-	@Override
-	public String getParameter(String name) {
-		String[] values = newMap.get(name);
-		if(values == null){
-			return null;
-		}else{
-			return values[0];
-		}
-		
-	}
-	
-	@Override
-	public Enumeration<String> getParameterNames() {
-		return Collections.enumeration(newMap.keySet());
-	}
-}
+//package kr.or.ddit.filter;
+//
+//import java.util.Enumeration;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletRequestWrapper;
+//
+//import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+//
+//public class PrameterWrapper extends HttpServletRequestWrapper {
+//	
+//	private Map<String, String[]> newMap;
+//	
+//	public PrameterWrapper(HttpServletRequest request) {
+//		super(request);
+//		
+//		//reuqest 객체의 parmeterMap객체를 복사하여 새로운 map객체를 생성
+//		newMap = new HashMap<String, String[]>(request.getParameterMap());
+//	}
+//	
+//	//새로운 파라미터를 저장한다.
+//	public void setParameter(String key, String[] values){
+//		newMap.put(key, values);
+//	}
+//	
+//	//parameter 관련 메소드 재정의
+//	//getParameter(String name);
+//	//getParameterValeus(String name);
+//	//getParameterMap();
+//	//getParameterNames();
+//	
+//	@Override
+//	public Map<String, String[]> getParameterMap() {
+//		return newMap;
+//	}
+//	
+//	public String[] getParameterValeus(String name) {
+//		return newMap.get(name);
+//	}
+//	
+//	@Override
+//	public String getParameter(String name) {
+//		String[] values = newMap.get(name);
+//		if(values == null){
+//			return null;
+//		}else{
+//			return values[0];
+//		}
+//		
+//	}
+//	
+//	@Override
+//	public Enumeration<String> getParameterNames() {
+//		return Collections.enumeration(newMap.keySet());
+//	}
+//}
